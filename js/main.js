9973e57d34188fc355093dac34f4f03483d14f92
Anton <neftnam@gmail.com>
2020-10-25 16:25:11 +0200

    added action attribute for the form

--- js/main.js
+++ js/main.js
@@ -16,64 +16,64 @@ document.addEventListener('DOMContentLoaded', function () {
 	logo();
 	//=========================Form=========================
 	const form = document.querySelector('#form');
-	form.addEventListener('submit', formSend);
-	async function formSend(e) {
-		e.preventDefault();
-		let error = formValidate(form);
-		// let formData = new FormData(form);
-		if (error === 0) {
-			// form.parentElement.classList.add('_sending');
-			// let response = await fetch('sendmail.php', {
-			// 	method: 'POST',
-			// 	body: formData
-			// });
-			// if (response.ok) {
-			// 	let result = await response.json();
-			// 	alert(result.message);
-			// 	form.reset();
-			// 	form.parentElement.classList.remove('_sending');
-			// } else {
-			// 	alert('Errorrr');
-			// 	form.parentElement.classList.remove('_sending');
-			// }
-		} else {
-			alert('Заполните все поля');
-		}
-	}
-	function formValidate(form) {
-		let error = 0;
-		let formReq = document.querySelectorAll('._req');
-		for (let index = 0; index < formReq.length; index++) {
-			const input = formReq[index];
-			formRemoveError(input);//!
-			if (input.classList.contains('_email')) {
-				if (emailTest(input)) {
-					formAddError(input);
-					error++;
-				}
-			} else {
-				if (input.value === '') {
-					formAddError(input);
-					error++;
-				}
-			}
-		}
-		return error;
-	}
-	//Вспомогательные ф-ии,добавляющие/удаляющее класс _error
-	function formAddError(input) {
-		input.parentElement.classList.add('_error');
-		input.classList.add('_error');
-	}
-	function formRemoveError(input) {
-		input.parentElement.classList.remove('_error');
-		input.classList.remove('_error');
-	}
+	// form.addEventListener('submit', formSend);
+	// async function formSend(e) {
+	// 	e.preventDefault();
+	// 	let error = formValidate(form);
+	// 	// let formData = new FormData(form);
+	// 	if (error === 0) {
+	// 		// form.parentElement.classList.add('_sending');
+	// 		// let response = await fetch('sendmail.php', {
+	// 		// 	method: 'POST',
+	// 		// 	body: formData
+	// 		// });
+	// 		// if (response.ok) {
+	// 		// 	let result = await response.json();
+	// 		// 	alert(result.message);
+	// 		// 	form.reset();
+	// 		// 	form.parentElement.classList.remove('_sending');
+	// 		// } else {
+	// 		// 	alert('Errorrr');
+	// 		// 	form.parentElement.classList.remove('_sending');
+	// 		// }
+	// 	} else {
+	// 		alert('Заполните все поля');
+	// 	}
+	// }
+	// function formValidate(form) {
+	// 	let error = 0;
+	// 	let formReq = document.querySelectorAll('._req');
+	// 	for (let index = 0; index < formReq.length; index++) {
+	// 		const input = formReq[index];
+	// 		formRemoveError(input);//!
+	// 		if (input.classList.contains('_email')) {
+	// 			if (emailTest(input)) {
+	// 				formAddError(input);
+	// 				error++;
+	// 			}
+	// 		} else {
+	// 			if (input.value === '') {
+	// 				formAddError(input);
+	// 				error++;
+	// 			}
+	// 		}
+	// 	}
+	// 	return error;
+	// }
+	// //Вспомогательные ф-ии,добавляющие/удаляющее класс _error
+	// function formAddError(input) {
+	// 	input.parentElement.classList.add('_error');
+	// 	input.classList.add('_error');
+	// }
+	// function formRemoveError(input) {
+	// 	input.parentElement.classList.remove('_error');
+	// 	input.classList.remove('_error');
+	// }
 
 
-	//Функция теста email
-	function emailTest(input) {
-		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-	}
+	// //Функция теста email
+	// function emailTest(input) {
+	// 	return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+	// }
 
 });
