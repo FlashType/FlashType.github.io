bcf5f46a93728a54fc2a6ddd5a3357b89a47ef9d
Anton <neftnam@gmail.com>
2020-10-25 16:13:43 +0200

    added action attribute for the form

--- js/main.js
+++ js/main.js
@@ -20,22 +20,22 @@ document.addEventListener('DOMContentLoaded', function () {
 	async function formSend(e) {
 		e.preventDefault();
 		let error = formValidate(form);
-		let formData = new FormData(form);
+		// let formData = new FormData(form);
 		if (error === 0) {
-			form.parentElement.classList.add('_sending');
-			let response = await fetch('sendmail.php', {
-				method: 'POST',
-				body: formData
-			});
-			if (response.ok) {
-				let result = await response.json();
-				alert(result.message);
-				form.reset();
-				form.parentElement.classList.remove('_sending');
-			} else {
-				alert('Errorrr');
-				form.parentElement.classList.remove('_sending');
-			}
+			// form.parentElement.classList.add('_sending');
+			// let response = await fetch('sendmail.php', {
+			// 	method: 'POST',
+			// 	body: formData
+			// });
+			// if (response.ok) {
+			// 	let result = await response.json();
+			// 	alert(result.message);
+			// 	form.reset();
+			// 	form.parentElement.classList.remove('_sending');
+			// } else {
+			// 	alert('Errorrr');
+			// 	form.parentElement.classList.remove('_sending');
+			// }
 		} else {
 			alert('Заполните все поля');
 		}
