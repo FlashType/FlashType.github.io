d56669bee355977ff0dc75021771802c2b1593f0
Anton <neftnam@gmail.com>
2020-11-13 09:12:03 +0200

    form XML req added

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+	//===================BURGER===================
 	function burger() {
 		document.querySelector('.menu-header__icon').addEventListener('click', () => {
 			document.querySelector('.menu-header__icon').classList.toggle('active');
@@ -8,21 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
 	}
 	burger();
 
-	// let tab = $('.tab-item');
-	// tab.on('click', function (event) {
-	// 	// $('.tab-item').click(function(event){
-	// 	$('.tab-content').removeClass('active');
-	// 	$('.tab-content[data-tab-name=' + $(this).attr('data-tab-name') + ']').toggleClass('active');
-
-	// });
 	//===============TABS===============
 	function tab() {
-
-
 		let tabHead = document.querySelectorAll('.tab-item'),
 			tabContent = document.querySelectorAll('.tab-content'),
 			tabName;
-		tabHead.forEach(item => { item.addEventListener('click', selectTabNav) });
+		tabHead.forEach(item => {
+			item.addEventListener('click', selectTabNav)
+		});
+
 		function selectTabNav() {
 			tabHead.forEach(item => {
 				item.classList.remove('active');
@@ -33,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 			selectTabContent(tabName);
 		}
-		function selectTabContent(tabName) {		
+		function selectTabContent(tabName) {
 			tabContent.forEach(item => {
 				console.log(item.getAttribute('data-tab-name'));
 				if (item.classList.contains(tabName)) {
@@ -57,5 +52,38 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	};
 	ibg();
-
+	//==============FORM==============
+	const form = document.querySelector('.form__body');
+	form.addEventListener('submit', formSend);
+	function formSend(e) {
+		e.preventDefault();
+		const message = {
+			success: "success",
+			failure: "failure",
+			loading: ""
+		};
+		const req = new XMLHttpRequest();
+		req.open('POST', 'https://formspree.io/f/mdopdn');
+		const formData = new FormData(form);
+		req.send(formData);
+		req.addEventListener('load', () => {
+			if (req.status === 200) {
+				form.reset();
+				statusMessage(form, message.success);
+			} else {
+				statusMessage(form, message.failure);
+			}
+		});
+	}
+	function statusMessage(form, message) {
+		form.classList.add('hide');
+		const messageBox = document.createElement('div');
+		messageBox.classList.add('form__message');
+		messageBox.innerHTML = `
+		<h3>${message}</h3>`;
+		form.parentElement.appent(messageBox);
+		setTimeout(() => {
+			statusMessage.remove();
+		}, 2000);
+	}
 });
